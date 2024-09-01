1a58f792bdc58d222d6959810d7e189618b94572
Htttpkaran <karankumar10986745@gmail.com>
2024-09-01 14:42:50 +0530

    Add operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "Karan"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
 
 
 // "33" => 33
@@ -14,7 +14,7 @@ console.log( valueInNumber);
 let isLoggedIn = "Karan"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false
 // "" => false
@@ -23,6 +23,12 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+// ********************** Operations ***********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+ 
