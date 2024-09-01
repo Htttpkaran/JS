926a62a9fa529d7b03e8dfd4dbb61f9bca006d9d
Htttpkaran <karankumar10986745@gmail.com>
2024-09-01 15:39:53 +0530

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console. log (2 > 1);
+// console. log (2 >= 1);
+// console. log (2 < 1);
+// console. log (2 == 1);
+// console. 1og (2 != 1);
+
+// console.log("2" > 1);
+// console.log ("02" > 1);
+
+// always avoid this type of comparisions
+// console.log (null > 0);
+// console.log (null== 0);
+// console.log (null >= 0);
+
+// console.log (undefined == 0);
+// console.log (undefined > 0);
+// console.log (undefined < 0);
+
+//===
+
+console.log ("2" === 2);
